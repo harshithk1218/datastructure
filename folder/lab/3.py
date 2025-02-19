5db68a6fa0766f5e84691f5c490ad4cc1b3cbe76
harshithk1218 <harshithkbca24@rvu.edu.in>
2025-02-19 10:59:06 +0530

    Update 3.py

--- folder/lab/3.py
+++ folder/lab/3.py
@@ -1,27 +1,8 @@
-def get_remainder_and_quotient(numbers):
-    remainders = []
-    quotients = []
-   
-    for num in numbers:
-        remainder = num % 10
-        quotient = num // 10  
-        remainders.append(remainder)
-        quotients.append(quotient)
-   
-    return remainders, quotients
-
-
-n = int(input("Enter the number of terms: "))
-
-
-numbers = []
-for i in range(n):
-    num = int(input(f"Enter number {i+1}: "))
-    numbers.append(num)
-
-
-remainders, quotients = get_remainder_and_quotient(numbers)
-
-
-print("Remainders:", remainders)
-print("Quotients:", quotients)
+def rec(n) :
+    if n==0:
+         return 0
+    else:
+          r=n%10  
+    return(r + rec(n//10))  
+n = int(input("enter a number"))
+print(rec(n))
