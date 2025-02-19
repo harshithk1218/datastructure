cf85303e0b04191ec96ec0cecd731900f18100b8
harshithk1218 <harshithkbca24@rvu.edu.in>
2025-02-19 11:06:14 +0530

    Update 3.py

--- folder/lab/3.py
+++ folder/lab/3.py
@@ -1,8 +1,14 @@
-def rec(n) :
-    if n==0:
-         return 0
+def rec(n):
+    if n == 0:
+        return 0
     else:
-          r=n%10  
-    return(r + rec(n//10))  
-n = int(input("enter a number"))
-print(rec(n))
+        remainder = n % 10  
+        quotient = n // 10
+       
+       
+        print(f"Number: {n}, Quotient: {quotient}, Remainder: {remainder}")
+       
+        return remainder + rec(quotient)  
+
+n = int(input("Enter a number: "))
+print("Sum of remainders:", rec(n))
